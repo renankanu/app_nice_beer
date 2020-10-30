8aebec3630bb921b4aade3f798cfbb5d1038bc7d
renankanu <renankanu@gmail.com>
2020-10-29 22:22:15 -0300

    Feature - create beer repository

--- lib/app/modules/home/home_module.dart
+++ lib/app/modules/home/home_module.dart
@@ -1,4 +1,7 @@
-import 'home_controller.dart';
+import 'package:app_nice_beer/app/modules/home/home_controller.dart';
+import 'package:app_nice_beer/app/repositories/beer_repository.dart';
+import 'package:dio/dio.dart';
+
 import 'package:flutter_modular/flutter_modular.dart';
 
 import 'home_page.dart';
@@ -7,6 +10,7 @@ class HomeModule extends ChildModule {
   @override
   List<Bind> get binds => [
         $HomeController,
+        Bind((i) => Dio()),
       ];
 
   @override
---
+++ lib/app/repositories/beer_repository.dart
@@ -0,0 +1,24 @@
+import 'package:app_nice_beer/app/models/beer_model.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+@Injectable()
+class BeerRepository extends Disposable {
+  final Dio _dio;
+
+  BeerRepository(this._dio);
+
+  String baseUrl = 'https://disease.sh/v3/covid-19';
+
+  Future<BeerModel> getRandomBeer() async {
+    try {
+      Response response = await _dio.get('$baseUrl/all');
+      return BeerModel.fromJson(response.data);
+    } on DioError catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  @override
+  void dispose() {}
+}
